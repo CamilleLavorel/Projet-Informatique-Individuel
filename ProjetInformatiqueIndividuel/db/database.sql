1eb9973fd9ca4a84def768e1d537a08dda0dae20
Camille Lavorel <clavorel001@ensc.fr>
2018-04-19 18:21:47 +0200

    Commit

--- ProjetInformatiqueIndividuel/db/database.sql
+++ ProjetInformatiqueIndividuel/db/database.sql
@@ -1,4 +1,4 @@
-create database if not exists microcms character set utf8 collate utf8_unicode_ci;
-use microcms;
+create database if not exists experienceetranger character set utf8 collate utf8_unicode_ci;
+use experienceetranger;
 
-grant all privileges on microcms.* to 'microcms_user'@'localhost' identified by 'secret';
+grant all privileges on experienceetranger.* to 'experience_user'@'localhost' identified by 'secret';
--- ProjetInformatiqueIndividuel/db/databasedb.sql
+++
@@ -1,4 +0,0 @@
-create database if not exists experienceetranger character set utf8 collate utf8_unicode_ci;
-use experienceetranger;
-
-grant all privileges on experienceetranger.* to 'experience_user'@'localhost' identified by 'secret';
